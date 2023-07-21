debee2b1fcfc0261bbbcc98d5edc47f443620251
Emerson Junior <emersonjr25@hotmail.com>
2023-07-21 18:11:53 -0300

    machine learning begin done

--- script/machine_learning_code.py
+++ script/machine_learning_code.py
@@ -4,11 +4,15 @@ THEME: MACHINE LEARNING SUPERVISIONED
 """
 
 ###### import data ######
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
-data_train = pd.read_csv('data/train.csv')
-data_test = pd.read_csv('data/test.csv')
+data_train = pd.read_csv('C:/Users/emers/OneDrive/Documentos/Supervisioned-Regression-Machine-Learning-main/data/train.csv')
+data_test = pd.read_csv('C:/Users/emers/OneDrive/Documentos/Supervisioned-Regression-Machine-Learning-main/data/test.csv')
 
 data_train.columns
 
@@ -38,5 +42,23 @@ data_train_X.dtypes
 
 #### machine learning ####
 
-X_train, X_val, y_train, y_val = train_test_split(data_train_X, data_train_Y, test_size = 0.3, random_state = 1)
+X_train, X_test, y_train, y_test = train_test_split(data_train_X, data_train_Y, test_size = 0.3, random_state = 1)
 
+#model 1
+model1 = LinearRegression()
+model1.fit(X_train, y_train)
+prediction1 = model1.predict(X_test)
+mse1 = mean_squared_error(y_test, prediction1)
+r2_1 = r2_score(y_test, prediction1)
+
+print('First model result mse:', mse1)
+print('First model result r2:', r2_1)
+
+#model 2
+model2 = RandomForestRegressor(n_estimators=100, random_state=1)
+model2.fit(X_train, y_train)
+prediction2 = model2.predict(X_test)
+mse2 = mean_squared_error(y_test, prediction2)
+r2_2 = r2_score(y_test, prediction2)
+print('Second model result mse:', mse2)
+print('Second model result r2:', r2_2)
